855ef1c52d547d9f5284626bff7a60fa2ae1bf0e
Yurii Z <akage.gd@gmail.com>
2020-10-19 17:14:29 +0300

    + add mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'eventbus_user',
+            password: '123',
+            database: 'eventbus',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
+
 })
 export class AppModule {
 }
