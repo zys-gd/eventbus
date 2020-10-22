a61080b9fc96a542b573248b055c540eb2bd10ba
Yurii Z <akage.gd@gmail.com>
2020-10-22 15:21:47 +0300

    * make sub apps

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,22 +4,9 @@ import {
     FastifyAdapter,
     NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import {Transport, MicroserviceOptions} from '@nestjs/microservices';
 
 (async function () {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({logger: true}));
     await app.listen(3000, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
-
-    // const rabbitmq = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    //     transport: Transport.RMQ,
-    //     options: {
-    //         urls: ['amqp://localhost:5672'],
-    //         queue: 'subscribers',
-    //         queueOptions: {
-    //             durable: false
-    //         },
-    //     },
-    // });
-    // rabbitmq.listen(() => console.log('Microservice is listening'));
 })();
--- apps/consumer/src/main.ts
+++ apps/consumer/src/main.ts
@@ -1,9 +1,17 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {MicroserviceOptions, Transport} from '@nestjs/microservices';
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-}
-
-bootstrap();
+(async function () {
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.RMQ,
+        options: {
+            urls: ['amqp://eventbus_rabbitmq:5672'],
+            queue: 'subscribers',
+            queueOptions: {
+                durable: false
+            },
+        },
+    });
+    app.listen(() => console.log('Microservice is listening'));
+})();
