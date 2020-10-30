ebc61b3d70153e9127a15026b7b0709472ee35e8
Yurii Z <akage.gd@gmail.com>
2020-10-30 12:01:36 +0200

    TPLV-1005: * improvements of db connection

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,27 +1,11 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {ConfigModule} from '@nestjs/config';
-import {AppConfigService} from "./app-config.service";
-import {AuthModule} from './auth/auth.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot(),
-        TypeOrmModule.forRootAsync({
-            imports: [
-                ConfigModule.forRoot(),
-                AppModule,
-            ],
-            inject: [AppConfigService],
-            useFactory: (config: AppConfigService) => config.dbConfig,
-        }),
-        AuthModule,
-    ],
     controllers: [AppController],
-    providers: [AppService, AppConfigService],
-    exports: [AppConfigService],
+    providers: [AppService,],
+    exports: [],
 })
 
 export class AppModule {
