d04d6c64563c6fad71a59eb3f9c8f5bea72114e7
Yurii Z <akage.gd@gmail.com>
2020-11-04 16:31:50 +0200

    TPLV-1005: * fix code style settings

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication, } from '@nestjs/platform-fastify';
 
 (async function () {
-    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({logger: true}));
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
     await app.listen(3000, '0.0.0.0');
     // eslint-disable-next-line no-console
     console.log(`Application is running on: ${await app.getUrl()}`);
