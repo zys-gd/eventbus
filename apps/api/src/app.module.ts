fdf2a8d7b9c770c2027299ad4d60122ae8c4441f
Yurii Z <akage.gd@gmail.com>
2020-12-02 15:31:57 +0200

    TPLV-1005: * change working with config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,37 +2,51 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { AppConfigService } from './app-config.service';
 import { AuthModule } from './auth/auth.module';
-import { ClientProxyFactory } from '@nestjs/microservices';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
-        TypeOrmModule.forRootAsync({
-            imports: [
-                ConfigModule.forRoot(),
-                AppModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            username: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            host: process.env.MYSQL_HOST,
+            port: Number(process.env.MYSQL_PORT),
+            debug: process.env.MYSQL_DEBUG === 'true' ? ['ComQueryPacket'] : false,
+            synchronize: true,
+            entities: [
+                './entities/*entity.ts'
             ],
-            inject: [AppConfigService],
-            useFactory: (config: AppConfigService) => config.dbConfig,
+            migrations: [
+                './migrations/*.ts'
+            ],
+            cli: {
+                entitiesDir: 'entities',
+                migrationsDir: 'migrations',
+            }
         }),
+        ClientsModule.register([
+            {
+                name: 'EVENT_SERVICE',
+                transport: Transport.RMQ,
+                options: {
+                    urls: [String(process.env.QUEUE_NAME)],
+                    queue: process.env.RABBITMQ_CONNECTION,
+                    queueOptions: {
+                        durable: false
+                    },
+                },
+            },
+        ]),
         AuthModule,
     ],
     controllers: [AppController],
     providers: [
         AppService,
-        AppConfigService,
-        {
-            provide: 'EVENT_SERVICE',
-            useFactory: (configService: AppConfigService) => {
-                return ClientProxyFactory.create(configService.rabbitmqConfig);
-            },
-            inject: [AppConfigService],
-        }
     ],
-    exports: [AppConfigService],
+    exports: [],
 })
 
 export class AppModule {
