d9d1578856e7b66e0fef8c16a4d530acbb070129
Svyatoslav Kubakh <svyatoslav.kubakh@gmail.com>
2021-03-30 19:37:54 +0300

    TAV-20 Event type id converted to int

--- src/common/entities/event-type.entity.ts
+++ src/common/entities/event-type.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('event_types')
 export class EventTypeEntity {
 
-    @PrimaryGeneratedColumn('uuid')
+    @PrimaryGeneratedColumn()
     uuid?: string;
 
     @Column()
