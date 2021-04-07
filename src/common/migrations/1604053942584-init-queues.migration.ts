5ee6fc859fcb30f228e48be94f3b2e5d96c32609
Svyatoslav Kubakh <svyatoslav.kubakh@gmail.com>
2021-04-07 20:30:51 +0300

    TAV-20 Queue migration improvements

--- src/common/migrations/1604053942584-init-queues.migration.ts
+++ src/common/migrations/1604053942584-init-queues.migration.ts
@@ -6,8 +6,14 @@ export class InitQueuesMigration1604053942584 implements MigrationInterface {
 
     public async up(): Promise<void> {
         const channel = await createQueueChannel(String(process.env.RABBITMQ_URL));
-        await createDelayedQueue(channel, String(process.env.EVENT_QUEUE_NAME));
-        await createDelayedQueue(channel, String(process.env.NOTIFICATION_QUEUE_NAME));
+        await Promise.all(
+            [
+                process.env.EVENT_QUEUE_NAME,
+                process.env.NOTIFICATION_QUEUE_NAME,
+            ].map(
+                queueName => createDelayedQueue(channel, String(queueName)),
+            )
+        );
     }
 
     public async down(): Promise<void> {
